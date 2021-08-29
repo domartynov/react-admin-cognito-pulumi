1f429d25140cbf70db727b0779c3cf83f627a144
Dmitry Martynov <domartynov@gmail.com>
2021-08-29 15:38:56 -0400

    initial pulumi project in /backend, incl. Lambda and Cognito

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # react-admin-cognito-pulumi
+
+TODO
+
+## Backend
+
+Components:
+* AWS API Lambda
+* AWS Cognito
+
+### Setup
+
+For Pulumi, see: 
+* [https://www.pulumi.com/docs/tutorials/aws/rest-api/]()
+* [https://www.pulumi.com/docs/guides/crosswalk/aws/api-gateway/]()
+
---
+++ backend/index.ts
@@ -0,0 +1,29 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+const project = pulumi.getProject()
+
+const api = new awsx.apigateway.API(`${project}-api`, {
+    routes: [{
+        path: "/",
+        method: "GET",
+        eventHandler: async (event) => {
+           return {
+               statusCode: 200,
+               body: "Hello!!!",
+           }
+        }
+    }],
+})
+
+
+const cognitoUserPool = new aws.cognito.UserPool(`${project}-pool`, {})
+const cognitoAppClient = new aws.cognito.UserPoolClient(`${project}-appclient`, {
+    userPoolId: cognitoUserPool.id
+})
+
+export const apiUrl = api.url
+export const cognitoUserPoolId = cognitoUserPool.id
+export const cognitoAppClientId = cognitoAppClient.id
+
